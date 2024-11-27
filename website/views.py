e4988277efec908e60816232711935ce20dbc176
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-27 19:52:40 +0500

    login

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+
+def home(request):
+   pass
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+  pass
